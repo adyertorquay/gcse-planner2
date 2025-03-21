87bfb546b6b9355200ddda0e8c90b46d379d55cf
adyertorquay <adyer@tqacademy.co.uk>
2025-03-21 19:09:55 +0000

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SubjectSetup from './pages/SubjectSetup';
+import Availability from './pages/Availability';
 import Timetable from './pages/Timetable';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Timetable />
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/subject-setup' element={<SubjectSetup />} />
+        <Route path='/availability' element={<Availability />} />
+        <Route path='/timetable' element={<Timetable />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
--- src/pages/Timetable.jsx
+++ src/pages/Timetable.jsx
@@ -1,5 +1,1 @@
-import React from 'react';
-
-export default function Timetable() {
-  return <h1 className="text-center mt-10 text-3xl text-blue-600">GCSE Planner App is Running 🎉</h1>;
-}
+export default function Timetable() { return <div className='text-center p-10 text-xl'>Timetable Page</div>; }
