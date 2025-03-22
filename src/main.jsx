c2757b18744228f06bb217f18e9e7ed943f700de
adyertorquay <adyer@tqacademy.co.uk>
2025-03-22 22:46:03 +0000

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import GCSEPlanner from './App.jsx';
 import './index.css';
-import GCSEPlanner from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
