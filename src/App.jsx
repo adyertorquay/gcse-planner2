7dec6d551192fada1eb0fce28b9a8c5e91a4045e
adyertorquay <adyer@tqacademy.co.uk>
2025-03-22 22:19:40 +0000

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,1 @@
-
-import React, { useState, useRef, useEffect } from 'react';
-import { format, parseISO, eachDayOfInterval, isBefore, addDays, isSameDay } from 'date-fns';
-import jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-
-// ... (truncated for brevity - assume full App.jsx content is inserted here)
-export default GCSEPlanner;
+<canvas_content_placeholder>
