56669041dd8bdf7c31f767003600c21cd70b583a
adyertorquay <adyer@tqacademy.co.uk>
2025-03-23 17:43:02 +0000

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import GCSEPlanner from './App.jsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GCSEPlanner from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<GCSEPlanner />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GCSEPlanner />
+  </React.StrictMode>,
+)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,7 @@
-module.exports = { content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'], theme: { extend: {} }, plugins: [] };
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
